a23eb017be573d66b446a97d89e5b5058d23c370
Kamal Maher <85698689+kmaherx@users.noreply.github.com>
2023-06-28 18:11:40 -0400

    typos

--- src/src/__init__.py
+++ src/src/__init__.py
@@ -2,4 +2,4 @@
 Spatially integrate spatially-resolved transcriptomics (SRT) datasets.
 """
 
-from .spin import spin
+from .src import spin
--- src/src/__main__.py
+++ src/src/__main__.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 import argparse
 
-from .spin import spin
+from .src import spin
 
 
 def main(
